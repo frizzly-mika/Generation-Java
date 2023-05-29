dbed054a20017ff056b1d8a9725d3b1ec296e219
frizzly-mika <micael.augustons@hotmail.com>
2023-05-29 20:34:11 -0300

    Atualização

--- src/lacosCondicionais/atividade4.java
+++ src/lacosCondicionais/atividade4.java
@@ -6,9 +6,9 @@ public class atividade4 {
 
 	public static void main(String[] args) {
 		String animal, aniClas1, aniClas1A,
-					   aniClas01, aniClas01A,
+					   aniClas01 = "", aniClas01A,
 					   aniClas2, aniClas2A,
-					   aniClas02, aniClas02A,
+					   aniClas02 = "", aniClas02A,
 											   vertClas1A = "Carnívoro", vertClas1Aa = "Águia",
 							vertClas1 = "Ave", vertClas1B = "Onívoro", vertClas1Bb = "Pomba",
 		vert = "Vertebrado", 
@@ -23,51 +23,51 @@ public class atividade4 {
  					 
 		Scanner leia = new Scanner(System.in);
 		
-		
 		System.out.println("Descubra qual é o animal");
 		System.out.println("\n'Vertebrado' ou 'Invertebrado'? Digite para saber:");
 		animal = leia.next();
 		
-		//vertebrados
-		if(animal == vert) {
+		//Vertebrados
+		if(animal.equalsIgnoreCase(vert)) {
 			System.out.println("'Ave' ou 'Mamífero'? Digite:");
 			aniClas1 = leia.next();
-			if(aniClas1 == vertClas1) {
+			if(aniClas1.equalsIgnoreCase(vertClas1)) {
 				System.out.println("'Carnívoro' ou 'Onívoro'? Digite:");
 				aniClas1A = leia.next();
-				if(aniClas1A == vertClas1A) {
+				if(aniClas1A.equalsIgnoreCase(vertClas1A)) {
 					System.out.println("O animal descoberto é o(a): "+ vertClas1Aa);
-				}else if(aniClas1A == vertClas1B) {
+				}else if(aniClas1A.equalsIgnoreCase(vertClas1B)) {
 							System.out.println("O animal descoberto é o(a): "+ vertClas1Bb);
 					
-			}else if(aniClas01 == vertClas2) {
+			}else if(aniClas01.equalsIgnoreCase(vertClas2)) {
 				System.out.println("'Onívoro' ou 'Herbívero'? Digite:");
 				aniClas01A = leia.next();
-				if(aniClas01A == vertClas2A) {
+				if(aniClas01A.equalsIgnoreCase(vertClas2A)) {
 					System.out.println("O animal descoberto é o(a): "+ vertClas2Aa);
-				}else if(aniClas01A == vertClas2B) {
+				}else if(aniClas01A.equalsIgnoreCase(vertClas2B)) {
 							System.out.println("O animal descoberto é o(a): "+ vertClas2Bb);
 					}
 				}
 			}
-			
-		}else if(animal == invert) {
+		
+		//Invertebrados
+		}else if(animal.equalsIgnoreCase(invert)) {
 			System.out.println("'Inseto' ou 'Anelídeo'? Digite:");
 			aniClas2 = leia.next();
-			if(aniClas2 == invertClas1) {
+			if(aniClas2.equalsIgnoreCase(invertClas1)) {
 				System.out.println("'Hematófago' ou 'Herbívoro'? Digite:");
 				aniClas2A = leia.next();
-				if(aniClas2A == invertClas1A) {
+				if(aniClas2A.equalsIgnoreCase(invertClas1A)) {
 					System.out.println("O animal descoberto é o(a): "+ invertClas1Aa);
-				}else if(aniClas2A == invertClas1B) {
+				}else if(aniClas2A.equalsIgnoreCase(invertClas1B)) {
 							System.out.println("O animal descoberto é o(a): "+ invertClas1Bb);
 					
-			}else if(aniClas02 == invertClas2) {
+			}else if(aniClas02.equalsIgnoreCase(invertClas2)) {
 				System.out.println("'Hematófago' ou 'Onívoro'? Digite:");
 				aniClas02A = leia.next();
-				if(aniClas02A == invertClas2A) {
+				if(aniClas02A.equalsIgnoreCase(invertClas2A)) {
 					System.out.println("O animal descoberto é o(a): "+ invertClas2Aa);
-				}else if(aniClas02A == invertClas2B) {
+				}else if(aniClas02A.equalsIgnoreCase(invertClas2B)) {
 							System.out.println("O animal descoberto é o(a): "+ invertClas2Bb);
 					}
 				}
