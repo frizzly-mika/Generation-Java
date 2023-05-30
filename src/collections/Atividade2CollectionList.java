a70051b701c855a4d08677db335ed4caec9c4dbe
frizzly-mika <micael.augustons@hotmail.com>
2023-05-30 17:03:24 -0300

    Aula 4 - Java

--- src/collections/Atividade2CollectionList.java
+++ src/collections/Atividade2CollectionList.java
@@ -11,7 +11,7 @@ public class Atividade2CollectionList {
 
 		ArrayList<Integer> lista = new ArrayList<Integer>();
 		Scanner leia = new Scanner(System.in);
-		Integer num = Integer.valueOf(1);
+		Integer num;
 		
 		lista.add(2); lista.add(5); lista.add(1); lista.add(3);lista.add(4);
 		lista.add(9); lista.add(7); lista.add(8); lista.add(10); lista.add(9);
