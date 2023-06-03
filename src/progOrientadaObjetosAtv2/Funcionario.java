511c208eae52d018fd53ee876b7438cbefcf8752
frizzly-mika <micael.augustons@hotmail.com>
2023-06-02 22:05:16 -0300

    Aula 8 - Java

---
+++ src/progOrientadaObjetosAtv2/Funcionario.java
@@ -0,0 +1,70 @@
+package progOrientadaObjetosAtv2;
+
+public class Funcionario {
+	
+	//Atributos da Classe Fliente
+		private String letra;
+		private String nome;
+		private int cpf;
+		private int telefone;
+		private String setor;
+		
+		//Método Construtor da Classa (+ parâmetros)
+		public Funcionario(String letra, String nome, int cpf, int telefone, String setor) {
+			this.letra = letra;
+			this.nome = nome;
+			this.cpf = cpf;
+			this.telefone = telefone;
+			this.setor = setor;
+		}	
+		
+		//Métodos Set = mudar dados do atributo e Get = ler dados do atributo
+		public String getLetra() {
+			return letra;
+		}
+		public void setLetra(String letra) {
+			this.letra = letra;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public int getCpf() {
+			return cpf;
+		}
+		public void setCpf(int cpf) {
+			this.cpf = cpf;
+		}
+		
+		public int getTelefone() {
+			return telefone;
+		}
+		public void setTelefone(int telefone) {
+			this.telefone = telefone;
+		}
+		
+		public String getSetor() {
+			return setor;
+		}
+		public void setSetor(String setor) {
+			this.setor = setor;
+		}
+		
+		//Método visualiza
+		public void visualiza() {
+
+			System.out.println("\n**************************************");
+			System.out.println("Dados do Funcionário");
+			System.out.println("\n**************************************");
+			System.out.println("\nLetra: " + this.letra);
+			System.out.println("\nNome: " + this.nome);
+			System.out.println("\nCPF: " + this.cpf);
+			System.out.println("\nTelefone: " + this.telefone);
+			System.out.println("\nSetor: " + this.setor);
+		}
+
+}
---
+++ src/progOrientadaObjetosAtv2/TestaFuncionario.java
@@ -0,0 +1,16 @@
+package progOrientadaObjetosAtv2;
+
+public class TestaFuncionario {
+
+	public static void main(String[] args) {
+
+		Funcionario funcionario1 = new Funcionario("A", "Jamile Rodriguez", 223355779, 923323223, "Contabilidade");
+		Funcionario funcionario2 = new Funcionario("B", "Mariana Meireles", 1105894182, 911112222, "Marketing");
+		
+		funcionario1.visualiza();
+		funcionario2.visualiza();
+		
+
+	}
+
+}
